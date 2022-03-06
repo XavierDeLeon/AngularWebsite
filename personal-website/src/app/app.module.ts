863b41fbcd789f4314c1f3e1d87b02ca4f6719a1
XavierDeLeon <ondevillaxavier@gmail.com>
2022-03-06 13:12:28 -0500

    Firebase fix attempt #4

--- personal-website/src/app/app.module.ts
+++ personal-website/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MenuComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
